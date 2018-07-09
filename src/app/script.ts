b172228f48508f57fc04b736a38126d9ce565d99
nish1111 <nishka.maharaj@chillisoft.co.za>
2018-07-09 11:43:09 +0200

    Nesting Components & inputs, User Interaction implemented- User can add Jokes

--- src/app/script.ts
+++ src/app/script.ts
@@ -1,12 +1,14 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {NgModule}      from '@angular/core';
+import {NgModule, EventEmitter, Input,Component}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {Component} from '@angular/core';
+import { Output } from '@angular/core';
+
+
 
 class Joke{
-  setup:string;
-  punchline:String;
-  hide:boolean;
+ public setup:string;
+ public punchline:String;
+ public hide:boolean;
 
   constructor(setup:string,punchline:string){
     this.setup= setup;
@@ -17,41 +19,92 @@ class Joke{
     this.hide=!this.hide;
   }
 }
-let joke= new Joke("What did the cheese say when it looked in the mirror?","Hello-Me (Halloumi)")
+@Component({
+  selector: 'joke-form',
+  template: `
+<div class="card card-block">
+  <h4 class="card-title">Create Joke</h4>
+  <div class="form-group">
+    <input type="text"
+           class="form-control"
+           placeholder="Enter the setup"
+           #setup>
+  </div>
+  <div class="form-group">
+    <input type="text"
+           class="form-control"
+           placeholder="Enter the punchline"
+           #punchline>
+  </div>
+  <button type="button"
+          class="btn btn-primary" (click)="createJoke(setup.value, punchline.value)">Create
+  </button>
+</div>
+  `
+})
+class JokeFormComponent{
+  @Output() jokeCreated= new EventEmitter<Joke>();
+  
+  createJoke(setup:string,punchline:string){
+    this.jokeCreated.emit(new Joke(setup,punchline));
+  }
+}
+
+@Component({
+  selector:'joke',
+  template:`
+  <div class="card card-block">
+    <h4 class="card-title">{{joke.setup}}</h4>
+    <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
+    <button class="btn btn-primary"
+    (click)="joke.toggle()">Tell me</button>
+  </div>
+    `
+}) 
+
+class JokeComponent{
+ @Input('joke') joke: Joke;
+
+}
+
 
 @Component({
   selector: 'joke-list',
   template: `
-<div class="card card-block"
-     *ngFor="let joke of jokes">
-  <h4 class="card-title">{{joke.setup}}</h4>
-  <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
-  <button class="btn btn-primary"
-  (click)="joke.toggle()">Tell me</button>
-</div>
+<joke-form (jokeCreated)="addJoke($event)"></joke-form>
+  <joke *ngFor="let j of jokes" [joke]="j"></joke>
   `
 })
-
 
 class JokeListComponent {
   jokes: Joke[];
 
-  constructor() {
+  constructor() { 
     this.jokes = [
       new Joke("What did the cheese say when it looked in the mirror?","Hello-Me (Halloumi)"),
       new Joke("What kind of cheese do you use to disguise a small horse?","Mask-a-pony (Mascarpone)"),
       new Joke("A kid threw a lump of cheddar at me","I thought ‘That’s not very mature’")
     ];
   }
-  toggle(joke){
-  joke.hide=!joke.hide();
+  addJoke(joke){
+    this.jokes.unshift(joke);
   }
 }
 
+@Component({
+  selector: 'app',
+  template: `
+  <joke-list></joke-list>
+  `
+})
+class AppComponent{
+    
+  }
+
 @NgModule({
   imports: [BrowserModule],
-  declarations: [JokeListComponent],
-  bootstrap: [JokeListComponent]
+  declarations: [AppComponent,JokeComponent, JokeListComponent, JokeFormComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
